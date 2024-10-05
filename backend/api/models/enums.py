3b63d2f2130aa62622dc279098d5662cdfe7895d
LuisGuilh3rme <luislebert01@outlook.com>
2024-10-05 15:46:21 -0300

    add enums model

---
+++ backend/api/models/enums.py
@@ -0,0 +1,16 @@
+import enum
+
+class AppointmentTypeEnum(enum.Enum):
+    routine_consultation = 0
+    specialty_appointment = 1
+    emergency_appointment = 2
+    return_appointment = 3
+    exam = 4
+
+class PaidEnum(enum.Enum):
+    pending = 0
+    paid = 1
+
+class SexEnum(enum.Enum):
+    male = 0
+    female = 1
