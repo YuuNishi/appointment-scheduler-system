76995d747af2532b70de949590d3df32d03323d5
LuisGuilh3rme <luislebert01@outlook.com>
2024-10-05 15:47:06 -0300

    add address schema and repository

---
+++ backend/api/schemas/address_schema.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, Field
+
+class AddressInput(BaseModel):
+    cep: str = Field(min_length=8, max_length=8)
+    street: str = Field(max_length=100)
+    neighborhood: str = Field(max_length=100)
+    city: str = Field(max_length=100)
+    federal_unit: str = Field(min_length=2, max_length=2)
+    number: int
+
+class AddressResponse(BaseModel):
+    id: int
