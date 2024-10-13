2475f908dfe197d71e991a0d95e9f4963ff30491
LuisGuilh3rme <luislebert01@outlook.com>
2024-10-13 18:47:38 -0300

    add address route with put and delete methods

--- backend/api/routers/api.py
+++ backend/api/routers/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from routers import patient
+from routers import patient, address
 
 router = APIRouter(prefix="/api")
 
 router.include_router(patient.router)
+router.include_router(address.router)
