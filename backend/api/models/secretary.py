c31967a9f0426f2f8436f4d62f1738313f0ad230
Jose Shibata <shibata_jose@hotmail.com>
2024-10-16 21:46:42 -0300

    Ajuste Modelo Atendente

--- backend/api/models/secretary.py
+++ backend/api/models/secretary.py
@@ -1,12 +1,14 @@
 from sqlalchemy import Column, ForeignKey, Date
+from sqlalchemy.orm import relationship
 from models.person import Person
 
 class Secretary(Person):
     __tablename__ = 'secretary'
 
     id = Column(ForeignKey('person.id'), primary_key=True,index=True)
-
     hiring_date = Column(Date)
+    
+    person = relationship('Person')
 
     __mapper_args__ = {
         'polymorphic_identity': 'secretary'
