6cdcb2a035fddc5f6e86b5b800d76d9cee23a3e1
Nishi <yumimnishi@gmail.com>
2024-11-06 21:08:24 -0300

    add: add style

--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -3,7 +3,7 @@
   import { imask } from '@imask/svelte';
   
   const options = { 
-		date: '00/00/0000',
+    date: '00/00/0000',
     cpf: '000.000.000-00',
     cep: '00000-000'
 	};
@@ -16,59 +16,101 @@
     <h1>Novo Paciente</h1>
     <form method="POST" action="?/register" >
       <p>Geral</p>
-      <div>
-        <label for="name">Nome:</label>
-        <input id="name"/>
+      <div class="row g-2 align-items-center">
+        <div class="col-auto">
+          <label for="name">Nome:</label>
+        </div>
+        <div class="col-md-6">
+          <input id="name" class="form-control"/>
+        </div>
       </div>
-      <div>
-        <label for="cpf">CPF:</label>
-        <input use:imask={options.cpf} type="text" id="cpf" placeholder="___.___.___-__"/>
-      </div>  
-      <div>
-        <label for="birthdate">Data Nascimento:</label>
-        <input use:imask={options.date} type="text" id="birthdate" placeholder="__/__/____"/>
-      </div>  
-      <div>
-        <label for="gender">Sexo:</label>
-          <select id="gender">
-              <option value="default">Selecione</option>
-              <option value=0>Feminino</option>
-              <option value=1>Masculino</option>
+      
+      <div class="row g-2 align-items-center">
+        <div class="col-auto">
+          <label for="cpf">CPF:</label>
+        </div>
+        <div class="col-md-2">
+          <input use:imask={options.cpf} class="form-control" type="text" id="cpf" placeholder="___.___.___-__"/>
+        </div>
+      </div>
+
+      <div class="row g-2 align-items-center">
+        <div class="col-auto">
+          <label for="birthdate">Data Nascimento:</label>
+        </div>
+        <div class="col-md-2">
+          <input use:imask={options.date} class="form-control" type="text" id="birthdate" placeholder="__/__/____"/>
+        </div>
+      </div>
+
+      <div class="row g-2 align-items-center">
+        <div class="col-auto">
+          <label for="gender">Sexo:</label>
+        </div>
+        <div class="col-md-2">
+          <select id="gender" class="form-control">
+            <option value="default">Selecione</option>
+            <option value=0>Feminino</option>
+            <option value=1>Masculino</option>
           </select> 
-        
-      </div> 
-      
-      
+        </div>
+      </div>
       <p>Endereço</p>
-      <div>
-        <label for="CEP">CEP:</label>
-        <input use:imask={options.cep} id="CEP" placeholder="_____-__"/>  
-      </div>  
-      <div class="form-row">
-          <div class="form-group col-md-6">
-            <label for="street">Rua:</label>
-            <input id="street"/>
-          </div>  
-          <div>
-            <label for="number">Número:</label> 
-            <input id="number" type="number"/>
-          </div>  
+
+      <div class="row g-2 align-items-center">
+        <div class="col-auto">
+          <label for="CEP">CEP:</label>
+        </div>
+        <div class="col-md-2">
+          <input use:imask={options.cep} id="CEP" class="form-control" placeholder="_____-__"/>
+        </div>
       </div>
-      <div>
-        <label for="neighborhood">Bairro:</label>
-        <input id="neighborhood"/>
+
+      <div class="row g-2 align-items-center">
+        <div class="col-auto">
+          <label for="street">Rua:</label>
+        </div>
+        <div class="col-md-6">
+          <input id="street" class="form-control"/>
+        </div>
+        <div class="col-auto">
+          <label for="number">Número:</label> 
+        </div>
+        <div class="col-md-2 ">
+          <input id="number" type="number" class="form-control"/>
+        </div>
       </div>
-      <div>
-        <label for="city">Cidade:</label>
-        <input id="city"/>
-      </div>  
-      <div>
-        <label for="state">Estado:</label>
-        <input id="state"/>
-      </div>  
-        
-        <button type="button" class="btn btn-default btn-outline" >Voltar sem salvar</button>
-        <button class="btn btn-success">salvar</button>
+
+      <div class="row g-2 align-items-center">
+        <div class="col-auto">
+          <label for="neighborhood">Bairro:</label> 
+        </div>
+        <div class="col-md-6">
+          <input id="neighborhood" type="text" class="form-control"/>
+        </div>
+      </div>
+
+      <div class="row g-2 align-items-center">
+        <div class="col-auto">
+          <label for="city">Cidade:</label>
+        </div>
+        <div class="col-md-4">
+          <input id="city" type="text" class="form-control"/>
+        </div>
+        <div class="col-auto">
+          <label for="state">Estado:</label>
+        </div>
+        <div class="col-md-auto">
+          <select id="state" class="form-control">
+            <option value="default">Selecione</option>
+            <option value=0>Feminino</option>
+            <option value=1>Masculino</option>
+          </select> 
+        </div>
+      </div>
+
+      <button type="button" class="btn btn-default btn-outline" >Voltar sem salvar</button>
+      <button class="btn btn-success">salvar</button>
     </form>  
   </div>
 </main>
@@ -77,7 +119,6 @@
   main {
     display: flex;
   }
-
   .content {
     flex: 1;
     background-color: #F0F0F0;
@@ -90,16 +131,14 @@
   p{
     color: #5398ec;
   }
-  div {
+  div.row {
     margin-bottom: 10px;
     margin-left: 2rem;
   }
+  
   label {
     display: inline-block;
     width: 150px;
     text-align: right;;
-  }
-  #name{
-    width: 65%;
   }
 </style>
