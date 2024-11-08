6b8323b96a04c8029e6c33591a7de07033db450e
Jose Shibata <shibata_jose@hotmail.com>
2024-11-08 19:37:58 -0300

    Tela de Login

--- backend/api/main.py
+++ backend/api/main.py
@@ -8,6 +8,8 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:8080",
+    "http://localhost:8001",
+    "http://localhost:5173",
 ]
 app.add_middleware(
     CORSMiddleware,
