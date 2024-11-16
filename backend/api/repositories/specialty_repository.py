df19362a3bef4ef0e1d875a04258599b98c905ca
LuisGuilh3rme <luislebert01@outlook.com>
2024-11-16 15:38:36 -0300

    implements specialty service with create and get endpoints

---
+++ backend/api/repositories/specialty_repository.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+from models.specialty import Specialty
+from schemas.specialty_schema import SpecialtyResponse
+
+class SpecialtyRepository:
+    def __init__(self, session: Session):
+      self.session = session
+    
+    def create(self, specialty: Specialty):
+      specialty = Specialty(**specialty.model_dump(exclude_none=True))
+      self.session.add(specialty)
+      self.session.commit()
+      self.session.refresh(specialty)
+      return SpecialtyResponse(**specialty.__dict__)
+        
+    def get_all(self):
+        specialties= self.session.query(Specialty).filter_by()
+        return specialties
