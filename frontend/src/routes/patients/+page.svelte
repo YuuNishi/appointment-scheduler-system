d240094e19e47367290ee6a94c1ee7b6e8b8ec06
Nishi <yumimnishi@gmail.com>
2024-11-26 00:36:29 -0300

    add: telas

---
+++ frontend/src/routes/patients/+page.svelte
@@ -0,0 +1,179 @@
+<script>
+  import Sidebar from '../../components/sidebar/sidebar.svelte';
+  import Edit from '../edit/+page.svelte'
+  import 'iconify-icon';
+
+  //-----------------
+  export let showModal = false;
+	let modalData;
+
+  export let edit;
+  export let form
+	function toggleModal(data) {
+		modalData = data;
+		showModal = !showModal;
+	}
+//----
+function search() {
+
+  var input, filter, table, tr, td,td2, i, txtValue,txtValue2;
+  input = document.getElementById("inputdefault");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("tb_patients");
+  tr = table.getElementsByTagName("tr");
+
+  //
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    td2= tr[i].getElementsByTagName("td")[1]
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      txtValue2 = td2.textContent || td2.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+///
+
+
+  export let data
+  
+  const {patients} = data
+
+  
+  let chunks = [];
+  
+  let count =1
+  for (let i = 0; i < patients.length; i += 6) {
+    let chunk = [];
+    for (let j = i; j < i + 6 && j < patients.length; j++) {
+        chunk.push(patients[j]);
+    }
+    chunks.push(chunk);
+  }
+  let patientPage = chunks[0]
+  function pagination(n){
+      patientPage= chunks[n]
+    
+  }
+  let isSorted = false
+function sortTable() {
+  if(!isSorted){
+    patientPage = patientPage.sort((a, b) => a.name.localeCompare(b.name))
+    isSorted = true
+  }else{
+    patientPage=patientPage.sort((a, b) => b.name.localeCompare(a.name))
+    isSorted= false
+  }
+}
+
+</script>
+
+<main>
+  <Sidebar />
+  <div class="content">
+    <h1>Prontuários</h1>
+    <div class="form-group">
+      <input class="form-control" id="inputdefault" type="text" placeholder="Digite o nome ou CPF" on:keyup={search}>
+    </div>
+
+    <div>
+      
+    </div>
+
+    <div class="results">
+      <div class="text-right">
+        <nav aria-label="Page navigation example">
+          <ul class="pagination">
+            {#each chunks as c, i}
+              <li class="page-item"><button class="page-link" on:click={()=>pagination(i)} >{i+1}</button></li>
+            {/each}
+          </ul>
+        </nav>
+        <button class="btn control" on:click={()=>sortTable()} >
+          <iconify-icon  icon="bi:filter" width="1.8em" height="1.8em" class="icon"></iconify-icon>
+        </button>
+        <a href="/register">
+          <button class="btn btn-success control">Novo Paciente</button>
+        </a>
+      </div>
+
+      <table class="table" id="tb_patients">
+        <thead>
+          <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">CPF</th>
+            <th scope="col">Sexo</th>
+            <th scope="col">Data de nascimento</th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each patientPage as person}
+            <tr>
+              <td>{person.name}</td>
+              <td>{person.cpf}</td>
+              {#if person.sex ==1}
+                <td>Feminino</td>
+              {:else}
+                <td>Masculino</td>
+              {/if}
+              <td>{person.birth_date.split("-").reverse().join("-")}</td>
+              <td>
+                  <iconify-icon  icon="subway:pencil" width="1.2em" on:click={()=>(toggleModal({person}))} height="1.2em" class="icon icon-edit"></iconify-icon>
+              </td>
+              <td>
+                <form method="post" action="?/deletePatient">
+                  <button type="submit" class="btn btn-default">
+                    <input type="text" id="id" name="id" hidden value={person.id}>
+                    <iconify-icon  icon="material-symbols:delete" width="1.2em" height="1.2em" class="icon icon-edit"></iconify-icon>
+                  </button>
+                </form>
+              </td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+      {#if showModal}
+	      
+        <Edit on:click={toggleModal} {edit} {form} bind:values={modalData} bind:show={showModal} />
+        
+      {/if}
+    </div>
+    
+  </div>
+
+</main>
+
+<style>
+  main {
+    display: flex;
+  }
+
+  .content {
+    flex: 1;
+    background-color: #F0F0F0;
+    padding: 20px;
+  }
+  .control{
+    float: right;
+    margin: 1%;
+  }
+  .icon{
+    color: rgb(61, 152, 255);
+  }
+  .results{
+    margin-top: 1rem;
+    background-color: #FFFFFF;
+    padding-top: 2rem;
+  }
+  .icon-edit{
+    align-self: right;
+    cursor: pointer;
+  }
+</style>
---
+++ frontend/src/routes/register/+page.svelte
@@ -0,0 +1,198 @@
+<script lang="ts">
+  import Sidebar from "../../components/sidebar/sidebar.svelte";
+  import { imask } from '@imask/svelte';
+  import {enhance} from '$app/forms'
+
+  export let form 
+  const options = { 
+    date: '00/00/0000',
+    cpf: '000.000.000-00',
+    cep: '00000-000'
+	};
+  const estates= [{nome:'Acre', sigla:'AC'},
+{nome:'Alagoas', sigla:'AL'},
+{nome:'Amapá', sigla:'AP'},
+{nome:'Amazonas', sigla:'AM'},
+{nome:'Bahia', sigla:'BA'},
+{nome:'Ceará', sigla:'CE'},
+{nome:'Espírito Santo', sigla:'ES'},
+{nome:'Goiás', sigla:'GO'},
+{nome:'Maranhão', sigla:'MA'},
+{nome:'Mato Grosso', sigla:'MT'},
+{nome:'Mato Grosso do Sul', sigla:'MS'},
+{nome:'Minas Gerais', sigla:'MG'},
+{nome:'Pará', sigla:'PA'},
+{nome:'Paraíba', sigla:'PB'},
+{nome:'Paraná', sigla:'PR'},
+{nome:'Pernambuco', sigla:'PE'},
+{nome:'Piauí', sigla:'PI'},
+{nome:'Rio de Janeiro', sigla:'RJ'},
+{nome:'Rio Grande do Norte', sigla:'RN'},
+{nome:'Rio Grande do Sul', sigla:'RS'},
+{nome:'Rondônia', sigla:'RO'},
+{nome:'Roraima', sigla:'RR'},
+{nome:'Santa Catarina', sigla:'SC'},
+{nome:'São Paulo', sigla:'SP'},
+{nome:'Sergipe', sigla:'SE'},
+{nome:'Tocantins', sigla:'TO'},
+{nome:'Distrito Federal', sigla:'DF'}
+]
+
+
+</script>
+
+<main>
+  <Sidebar />
+  <div class="content">
+    <h1>Novo Paciente</h1>
+    <form method="POST" action="?/create" use:enhance={()=>{return async({update})=>{update({reset: false})}}} >
+      <p class="section">Geral</p>
+      <div class="row g-2 align-items-center">
+        <div class="col-auto">
+          <label for="name">Nome:</label>
+        </div>
+        <div class="col-md-6">
+          <input id="name" name="name" required class="form-control"/>
+        </div>
+      </div>
+      
+      <div class="row g-2 align-items-center">
+        <div class="col-auto">
+          <label for="cpf">CPF:</label>
+        </div>
+        <div class="col-md-2">
+          <input use:imask={options.cpf} name="cpf" minlength="14" required class="form-control" type="text" id="cpf" placeholder="___.___.___-__"/>
+        </div>
+      </div>
+
+      <div class="row g-2 align-items-center">
+        <div class="col-auto">
+          <label for="birthdate">Data Nascimento:</label>
+        </div>
+        <div class="col-md-2">
+          <input use:imask={options.date} class="form-control" required type="text" name="birthdate" id="birthdate" placeholder="__/__/____"/>
+          {#if form?.invalid}
+            <p class="error" >Data inválida</p>
+          {/if}
+        </div>
+        
+      </div>
+
+      <div class="row g-2 align-items-center">
+        <div class="col-auto">
+          <label for="gender">Sexo:</label>
+        </div>
+        <div class="col-md-2">
+          <select id="gender" name="gender" required class="form-control">
+            <option value="" selected disabled hidden >Selecione</option>
+            <option value=0>Masculino</option>
+            <option value=1>Feminino</option>
+          </select> 
+        </div>
+      </div>
+      <p class="section">Endereço</p>
+
+      <div class="row g-2 align-items-center">
+        <div class="col-auto">
+          <label for="cep">CEP:</label>
+        </div>
+        <div class="col-md-2">
+          <input use:imask={options.cep} id="cep" minlength="8" name="cep" required class="form-control" placeholder="_____-__"/>
+        </div>
+      </div>
+
+      <div class="row g-2 align-items-center">
+        <div class="col-auto">
+          <label for="street">Rua:</label>
+        </div>
+        <div class="col-md-6">
+          <input id="street" name="street" required class="form-control"/>
+        </div>
+        <div class="col-auto">
+          <label for="number">Número:</label> 
+        </div>
+        <div class="col-md-2 ">
+          <input id="number" type="number" required name="number" class="form-control"/>
+        </div>
+      </div>
+
+      <div class="row g-2 align-items-center">
+        <div class="col-auto">
+          <label for="neighborhood">Bairro:</label> 
+        </div>
+        <div class="col-md-6">
+          <input id="neighborhood" name="neighborhood" required type="text" class="form-control"/>
+        </div>
+      </div>
+
+      <div class="row g-2 align-items-center">
+        <div class="col-auto">
+          <label for="city">Cidade:</label>
+        </div>
+        <div class="col-md-4">
+          <input id="city" name="city" type="text" required class="form-control"/>
+        </div>
+        <div class="col-auto">
+          <label for="state">Estado:</label>
+        </div>
+        <div class="col-md-auto">
+          <select id="state" name="state" required class="form-control">
+            <option value="" selected disabled hidden >Selecione</option>
+            {#each estates as estate}
+              <option value={estate.sigla}>{estate.nome}</option>
+            {/each}
+          </select> 
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col align-self-end">
+          <a href="/patients">
+            <button type="button" class="btn btn-default btn-outline" >Voltar sem salvar</button>
+          </a>
+          <button class="btn btn-success" type="submit">Salvar</button>
+        </div>
+      </div>
+      
+      
+    </form>  
+  </div>
+</main>
+
+<style>
+  main {
+    display: flex;
+  }
+  .content {
+    flex: 1;
+    background-color: #F0F0F0;
+    padding: 20px;
+  }
+  form{
+    background-color: white;
+    padding: 2rem;
+  }
+  p.section{
+    color: #5398ec;
+  }
+  p.error{
+    color: red;
+  }
+  div.row {
+    margin-bottom: 10px;
+    margin-left: 2rem;
+  }
+  
+  label {
+    display: inline-block;
+    width: 150px;
+    text-align: right;;
+  }
+  .btn-default{
+    border-color: black;
+  }
+  .btn-default:hover {
+    background-color:#dfdede;
+    transition: 0.7s;
+  }
+</style>
