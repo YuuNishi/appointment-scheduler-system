0715197fb8bc5aa89c2fa2d5f92b0a00816c99ab
LuisGuilh3rme <luislebert01@outlook.com>
2024-11-25 21:23:01 -0300

    implements doctor delete endpoint

--- backend/api/schemas/doctor_schema.py
+++ backend/api/schemas/doctor_schema.py
@@ -1,9 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
 
 class DoctorInput(BaseModel):
-    name: str
+    name: str = Field(min_length=1, max_length=100)
     speciality_id: int
 
 class DoctorResponse(BaseModel):
     id: int
     name: str
+    specialty: Optional[str] = None
